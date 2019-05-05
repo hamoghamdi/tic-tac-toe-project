31c9e6fd5721a4e72c46a8955eabdd37c2031e34
hamoghamdi <haneen.aljabrah@gmail.com>
2019-05-05 16:01:09 +0300

    js: done with all winning scenarios scenario 1: row, scenario 2: column, scenario 3: diagonal left requirments: 1- tie, 2- who's turn is it, 3- KISS, DRY html: complete but missing paragraph for who's turn is it

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 // play
-// calc winner
+// calc winner 
+// scenario 1 : all col clicked by one player
+// scenario 2 : all row clicked by one player
 // reset game
 
 let turn = true;
-// if(turn) => change to X 
-// else change to O 
+// if(turn) => X 
+// else => O 
 
 // an array to save all tics ? 
 
 let checkedTics = [];
-let show;
+// let show;
 $('.tic').one('click', function(event){
     event.preventDefault()
     if (turn) {
@@ -25,20 +27,110 @@ $('.tic').one('click', function(event){
 }
 })
 
-// check col winnig scenarios
 // scenario 1 : if (all col-1 || all col-2 || all col-3 children) has been clicked
-
 $('.tic').one('click', function (event) {
-    // console.log('col-1 clicked'); // this would log three times 
-    // i wanna check if all three are clicked 
     if ($('#col-1').children().text() == 'XXX' || $('#col-2').children().text() == 'XXX' || $('#col-3').children().text() == 'XXX') {
-        // console.log('all col-1 clicked by X');
+        // console.log('all col clicked by X');
         $("#winner").text(' Player X');
         $('.tic').off('click');
     } else {
         if ($('#col-1').children().text() == 'OOO' || $('#col-2').children().text() == 'OOO' || $('#col-3').children().text() == 'OOO'){
-            // console.log('all col-1 clicked by O');
+            // console.log('all col clicked by O');
             $("#winner").text(' Player O');
             $('.tic').off('click');}
     }
-}) // this happens to col-2 and col-3 , in total 3 scenarios
+})
+
+// scenario 2 : all row clicked by one player
+let rowWin = ['row', 'row', 'row'];
+$('.tic').one('click', function (event) {
+    if ($(event.target).index() == 0) 
+    {
+    // console.log(' 1st row clicked') // log three times 
+    // are all X
+    rowWin[0] += $(event.target).text();
+    // console.log(rowWin);
+    } else {
+        if ($(event.target).index() == 1) {
+            // console.log(' 2nd row clicked') // log three times 
+            rowWin[1] += $(event.target).text();
+            // console.log(rowWin);
+        } else {
+            if ($(event.target).index() == 2) {
+                // console.log(' 3rd row clicked') // log three times 
+                // are all X
+                rowWin[2] += $(event.target).text();
+                // console.log(rowWin);
+            }
+        }
+
+    }
+
+    // calc row winner 
+    for (let i =0; i<rowWin.length; i++){
+        if (rowWin[i] == 'rowOOO'){
+            $("#winner").text(' Player O');
+            $('.tic').off('click');
+        } else {
+            if (rowWin[i] == 'rowXXX'){
+                $("#winner").text(' Player X');
+                $('.tic').off('click'); 
+            }
+        }
+    }
+})
+
+let diagonalWin = ['d', 'd'];
+// scenario 3 : diagonal win
+// index == 0 && col-1 || index == 0 && col-3
+// index == 1 && col-2
+// index == 2 && col-1 || index == 2 && col-3
+$('.tic').one('click', function (event) {
+    if ($(event.target).index() == 0 && $(event.target).parent('.col').attr('id') == 'col-1') {
+        diagonalWin[0] += $(event.target).text();
+        // console.log(diagonalWin);
+        } else {
+        if ($(event.target).index() == 0 && $(event.target).parent('.col').attr('id') == 'col-3'){
+                diagonalWin[1] += $(event.target).text();
+                // console.log(diagonalWin);  
+            }
+        }
+     
+    if ($(event.target).index() == 1 && $(event.target).parent('.col').attr('id') == 'col-2'){
+            diagonalWin[0] += $(event.target).text();
+            diagonalWin[1] += $(event.target).text();
+            // console.log(diagonalWin);
+        }
+    if ($(event.target).index() == 2 && $(event.target).parent('.col').attr('id') == 'col-1'){
+            diagonalWin[1] += $(event.target).text();
+            // console.log(diagonalWin); 
+            } else {
+        if ($(event.target).index() == 2 && $(event.target).parent('.col').attr('id') == 'col-3'){
+                    diagonalWin[0] += $(event.target).text();
+                    // console.log(diagonalWin); 
+                    }
+                }
+// calc diagonal win
+for (let i=0; i<diagonalWin.length; i++){
+    if (diagonalWin[i] == 'dXXX'){
+        $("#winner").text(' Player X');
+        $('.tic').off('click'); 
+    } else {
+        if (diagonalWin[i] == 'dOOO'){
+            $("#winner").text(' Player O');
+            $('.tic').off('click');  
+        }
+    }
+}
+            
+})
+
+$('#reset').on('click', function(event){
+    location.reload();
+})
+
+// requirments:
+// show if tie
+// on each turn, who's turn is it
+
+// KISS, DRY
