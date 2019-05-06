04cf8bf0e31f8ead675a18bc8375df4fef2271d7
hamoghamdi <haneen.aljabrah@gmail.com>
2019-05-06 09:39:16 +0300

    commit the good work before any big change

--- js/main.js
+++ js/main.js
@@ -8,22 +8,26 @@ let turn = true;
 // if(turn) => X 
 // else => O 
 
-// an array to save all tics ? 
+const winnerIsX = function(event){
+    $("#winner").text(' Player X');
+    $('.tic').off('click');
+}
 
-let checkedTics = [];
-// let show;
+const winnerIsO = function(event){
+    $("#winner").text(' Player O');
+    $('.tic').off('click');
+}
+// an array to save all tics ? 
+// let checkedTics = [];
+
 $('.tic').one('click', function(event){
     event.preventDefault()
     if (turn) {
         $(event.target).html('<span>X</span>')
         turn = false;
-        // show = $(event.target).index();
-        // console.log(show);
     }else {
         $(event.target).html('<span>O</span>')
         turn = true;
-        // show = $(event.target).index();
-        // console.log(show)
 }
 })
 
@@ -31,94 +35,88 @@ $('.tic').one('click', function(event){
 $('.tic').one('click', function (event) {
     if ($('#col-1').children().text() == 'XXX' || $('#col-2').children().text() == 'XXX' || $('#col-3').children().text() == 'XXX') {
         // console.log('all col clicked by X');
-        $("#winner").text(' Player X');
-        $('.tic').off('click');
+        winnerIsX(event);
     } else {
         if ($('#col-1').children().text() == 'OOO' || $('#col-2').children().text() == 'OOO' || $('#col-3').children().text() == 'OOO'){
             // console.log('all col clicked by O');
-            $("#winner").text(' Player O');
-            $('.tic').off('click');}
+            winnerIsO(event); }
     }
 })
 
 // scenario 2 : all row clicked by one player
-let rowWin = ['row', 'row', 'row'];
+let rowWinArray = ['row', 'row', 'row'];
 $('.tic').one('click', function (event) {
     if ($(event.target).index() == 0) 
     {
     // console.log(' 1st row clicked') // log three times 
     // are all X
-    rowWin[0] += $(event.target).text();
-    // console.log(rowWin);
+    rowWinArray[0] += $(event.target).text();
+    // console.log(rowWinArray);
     } else {
         if ($(event.target).index() == 1) {
             // console.log(' 2nd row clicked') // log three times 
-            rowWin[1] += $(event.target).text();
-            // console.log(rowWin);
+            rowWinArray[1] += $(event.target).text();
+            // console.log(rowWinArray);
         } else {
             if ($(event.target).index() == 2) {
                 // console.log(' 3rd row clicked') // log three times 
                 // are all X
-                rowWin[2] += $(event.target).text();
-                // console.log(rowWin);
+                rowWinArray[2] += $(event.target).text();
+                // console.log(rowWinArray);
             }
         }
 
     }
 
     // calc row winner 
-    for (let i =0; i<rowWin.length; i++){
-        if (rowWin[i] == 'rowOOO'){
-            $("#winner").text(' Player O');
-            $('.tic').off('click');
+    for (let i =0; i<rowWinArray.length; i++){
+        if (rowWinArray[i] == 'rowOOO'){
+            winnerIsO(event);
         } else {
-            if (rowWin[i] == 'rowXXX'){
-                $("#winner").text(' Player X');
-                $('.tic').off('click'); 
+            if (rowWinArray[i] == 'rowXXX'){
+                winnerIsX(event);
             }
         }
     }
 })
 
-let diagonalWin = ['d', 'd'];
+let diagonalWinArray = ['d', 'd'];
 // scenario 3 : diagonal win
 // index == 0 && col-1 || index == 0 && col-3
 // index == 1 && col-2
 // index == 2 && col-1 || index == 2 && col-3
 $('.tic').one('click', function (event) {
     if ($(event.target).index() == 0 && $(event.target).parent('.col').attr('id') == 'col-1') {
-        diagonalWin[0] += $(event.target).text();
-        // console.log(diagonalWin);
+        diagonalWinArray[0] += $(event.target).text();
+        // console.log(diagonalWinArray);
         } else {
         if ($(event.target).index() == 0 && $(event.target).parent('.col').attr('id') == 'col-3'){
-                diagonalWin[1] += $(event.target).text();
-                // console.log(diagonalWin);  
+                diagonalWinArray[1] += $(event.target).text();
+                // console.log(diagonalWinArray);  
             }
         }
      
     if ($(event.target).index() == 1 && $(event.target).parent('.col').attr('id') == 'col-2'){
-            diagonalWin[0] += $(event.target).text();
-            diagonalWin[1] += $(event.target).text();
-            // console.log(diagonalWin);
+            diagonalWinArray[0] += $(event.target).text();
+            diagonalWinArray[1] += $(event.target).text();
+            // console.log(diagonalWinArray);
         }
     if ($(event.target).index() == 2 && $(event.target).parent('.col').attr('id') == 'col-1'){
-            diagonalWin[1] += $(event.target).text();
-            // console.log(diagonalWin); 
+            diagonalWinArray[1] += $(event.target).text();
+            // console.log(diagonalWinArray); 
             } else {
         if ($(event.target).index() == 2 && $(event.target).parent('.col').attr('id') == 'col-3'){
-                    diagonalWin[0] += $(event.target).text();
-                    // console.log(diagonalWin); 
+                    diagonalWinArray[0] += $(event.target).text();
+                    // console.log(diagonalWinArray); 
                     }
                 }
 // calc diagonal win
-for (let i=0; i<diagonalWin.length; i++){
-    if (diagonalWin[i] == 'dXXX'){
-        $("#winner").text(' Player X');
-        $('.tic').off('click'); 
+for (let i=0; i<diagonalWinArray.length; i++){
+    if (diagonalWinArray[i] == 'dXXX'){
+        winnerIsX(event);
     } else {
-        if (diagonalWin[i] == 'dOOO'){
-            $("#winner").text(' Player O');
-            $('.tic').off('click');  
+        if (diagonalWinArray[i] == 'dOOO'){
+            winnerIsO(event);  
         }
     }
 }
@@ -134,3 +132,12 @@ $('#reset').on('click', function(event){
 // on each turn, who's turn is it
 
 // KISS, DRY
+const columnWin = function(event){
+
+}
+const rowWin = function(event){
+
+}
+const diagonalWin = function(event){
+
+}
